ca6bd6ebdcf177153cb7a563cfd4124d9a3fc015
KokoKrumov <koko771@abv.bg>
2014-11-26 15:57:07 +0200

    Add directorys and files in views and assets

---
+++ application/views/site/NAORozen.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>NAO Rozhen</title>
+		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link rel="shortcut icon" href="images/NAOR.png">
+		<link rel="stylesheet" href="css/cssReset.css">
+		<link rel="stylesheet" href="css/RozhenStyles.css">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	</head>
+	<body>
+	<!-- HEADER of the Page -->
+		<header>
+		</header>
+	<!-- CONTENT of the Page starts from here-->
+		<div id="content">
+		</div>
+		
+	<!-- FOOTER of the Page -->
+		<footer>	
+		</footer>
+		
+		<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	</body>
+</html>

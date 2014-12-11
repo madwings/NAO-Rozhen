a405cdbcac04134bc3e6f6d6c780103c065a534f
KokoKrumov <koko771@abv.bg>
2014-12-11 13:47:26 +0200

    add images,css and js files for social bar

--- application/views/site/header.php
+++ application/views/site/header.php
@@ -6,9 +6,14 @@
 		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<link rel="icon" href="favicon.ico">
-		<link rel="stylesheet" href="assets/css/reset.css">
 		<link rel="stylesheet" href="assets/css/main.css">
 		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+		
+		<!--JS-->
+		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="assets/js/plugins.js"></script>
+		<script type="text/javascript" src="assets/js/custom.js"></script>
+		
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -18,6 +23,55 @@
 	<body>
 		<!-- HEADER of the Page -->
 			<header>
+				<!-- ============================================
+										top bar
+					============================================= -->
+				<div id="top-bar">
+					<div class="container clearfix"  >
+						<div id="top-social" >
+							<ul>
+
+								<li class="ts-facebook">
+									<a href="https://www.facebook.com/SaveNAORozhen/timeline" target="_blank">
+										<div class="ts-icon"></div>
+										<div class="ts-text">Facebook</div>
+									</a>
+								</li>
+								
+								<li class="ts-gplus">
+									<a href="#" rel="publisher" target="_blank">
+										<div class="ts-icon"></div>
+										<div class="ts-text">Google+</div>
+									</a>
+								</li>
+								<!--<li class="ts-pinterest">
+									<a href="#" target="_blank">
+										<div class="ts-icon"></div>
+										<div class="ts-text">Pinterest</div>
+									</a>
+								</li>
+								<li class="ts-twitter">
+									<a href="#">
+										<div class="ts-icon"></div>
+										<div class="ts-text">Twitter</div>
+									</a>
+								</li>-->
+								<li class="ts-phone">
+									<a href="http://www.astro.bas.bg/admin_bg.html">
+										<div class="ts-icon"></div>
+										<div class="ts-text">administration</div>
+									</a>
+								</li>
+								<li class="ts-mail">
+									<a href="mailto:observations@astro.bas.bg">
+										<div class="ts-icon"></div>
+										<div class="ts-text">observations@astro.bas.bg</div>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
 			</header>
 		<!-- CONTENT of the Page starts from here-->
 			<div id="content">

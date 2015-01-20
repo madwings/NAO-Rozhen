30e33bce04e45a241d51b7f793e4e2a30a4af163
KokoKrumov <koko771@abv.bg>
2015-01-20 20:14:01 +0200

    create header of the page

--- application/views/site/header.php
+++ application/views/site/header.php
@@ -1,89 +1,103 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title><?php echo lang('title'); ?></title>
-		<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<link rel="stylesheet" href="assets/css/main.css">
-		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-		
-		<!--JS-->
-		<script type="text/javascript" src="assets/js/jquery.min.js"></script>
-		<script type="text/javascript" src="assets/js/plugins.js"></script>
-		<script type="text/javascript" src="assets/js/custom.js"></script>
-		
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	</head>
-	<body>
-		<!-- HEADER of the Page -->
-			<header>
-				<!-- ============================================
-										top bar
-					============================================= -->
-				<div id="top-bar">
-					<div class="container clearfix"  >
-						<div id="top-social" >
-							<ul>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo lang('title'); ?></title>
+	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-								<li class="ts-facebook">
-									<a href="https://www.facebook.com/SaveNAORozhen/timeline" target="_blank">
-										<div class="ts-icon"></div>
-										<div class="ts-text">Facebook</div>
-									</a>
-								</li>
-								
-								<li class="ts-gplus">
-									<a href="#" rel="publisher" target="_blank">
-										<div class="ts-icon"></div>
-										<div class="ts-text">Google+</div>
-									</a>
-								</li>
-								<!--<li class="ts-pinterest">
-									<a href="#" target="_blank">
-										<div class="ts-icon"></div>
-										<div class="ts-text">Pinterest</div>
-									</a>
-								</li>
-								<li class="ts-twitter">
-									<a href="#">
-										<div class="ts-icon"></div>
-										<div class="ts-text">Twitter</div>
-									</a>
-								</li>-->
-								<li class="ts-phone">
-									<a href="http://www.astro.bas.bg/admin_bg.html">
-										<div class="ts-icon"></div>
-										<div class="ts-text">administration</div>
-									</a>
-								</li>
-								<li class="ts-mail">
-									<a href="mailto:observations@astro.bas.bg">
-										<div class="ts-icon"></div>
-										<div class="ts-text">observations@astro.bas.bg</div>
-									</a>
-								</li>
-							</ul>
+	<!--CSS-->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	
+	<!--JS-->
+	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
+	
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!-- HEADER of the Page -->
+		<header>
+			<div class="headerContent">
+				<div class="container">
+					<div class="row  text-uppercase">
+						<div class="col-lg-1 col-lg-offset-11" id="anchors">
+							<a href="#">bg</a>
+							<a href="#">en</a>
+						</div>
+					</div>
+				
+					<div class="row" id="NAOHeader">
+						<div class="col-lg-8 col-lg-offset-2">
+							<p class="text-uppercase text-center" id="NAOHeaderTitle">
+							национална астрономическа обсерватория - рожен</p>
+						</div>
+						<div id="clocks" class="col-lg-2">
+							<div class="col-lg-6">
+								<img src="assets/images/icons/clock.png" alt="">
+								<p>EET</p>
+							</div>
+							<div class="col-lg-6">
+								<img src="assets/images/icons/clock.png" alt="">
+								<p>UT</p>
+							</div>
 						</div>
 					</div>
 				</div>
-			</header>
-		<!-- CONTENT of the Page starts from here-->
-			<div id="content">
-			</div>
-			
-		<!-- FOOTER of the Page -->
-			<footer>	
-			</footer>
-			
-			<!-- Bootstrap core JavaScript
-		================================================== -->
-		<!-- Placed at the end of the document so the pages load faster -->
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
-	</body>
+					<!--NAVIGATION-->
+					<div  class="navBorderTop"></div>
+					<nav class="navbar text-uppercase" role="navigation" id="nav">
+						<div class="container" id="navContainer">
+							<div class="row">
+								<div class="col-lg-10 col-lg-offset-1">
+									<div class="navbar-header">
+									<!--button for responsive navigation -->
+										<button type="button" class="navbar-toggle collapsed"  data-toggle="collapse" data-target="#navigation">BUTTON
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+										</button>
+									</div>
+									<div class="collapse navbar-collapse" id="navigation">
+										<ul class="nav navbar-nav ">
+											<li><a href="#">начало</a></li>
+											<li><a href="#">новини</a></li>
+											<li><a href="#">обсерваторията</a></li>
+											<li><a href="#">институт</a></li>
+											<li><a href="#">астро-календар</a></li>
+											<li><a href="#">галерии</a></li>
+											<li><a href="#">контакти</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</nav><!--END OF NAVIGATION-->
+				<div  class="navBorderTop"></div>
+			</div><!--END OF HEDERCONTENT-->
+		</header>
+	<!-- CONTENT of the Page starts from here-->
+		<div id="content">
+		
+		</div>
+		
+	<!-- FOOTER of the Page -->
+		<footer>	
+		</footer>
+		
+		<!-- Bootstrap core JavaScript
+	================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</body>
 </html>

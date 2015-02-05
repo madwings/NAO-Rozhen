34d365aeca238928333d11e883a86c82b682d799
madwings <stily_c@yahoo.com>
2015-02-06 00:53:32 +0200

    multilanguage mods and style fixes;
    added browser`s accepted languages detection;
    
    Signed-off-by: madwings <stily_c@yahoo.com>

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -21,14 +21,18 @@ class Main extends CI_Controller {
 	}
 	
 	private function _language_detect() {
+		$headers_all = getallheaders();
 		if ( ! empty($_COOKIE['site_lang'])) {
 			$this->language = $_COOKIE['site_lang'];
+		} else if ( ! empty($headers_all['Accept-Language']) && strpos($headers_all['Accept-Language'], 'bg') !== FALSE) {
+			$this->language = 'bulgarian';
 		} else {
 			$this->language = 'english';
 		}
 	}
+	
 	public function language_switch($language = NULL) {
-		$language = $language !== NULL ? $language : 'bulgarian';
+		$language = $language !== NULL ? $language : 'english';
 		// Set language cookie to last for an year from now
 		setcookie("site_lang", $language, time() + 31536000, '/');
 		echo json_encode(array('lang' => $language));
--- application/language/english/home_lang.php
+++ application/language/english/home_lang.php
@@ -1,7 +1,7 @@
 <?php  defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['upcoming_events'] = 'upcoming events';
-$lang['comets'] = 'comets';
+$lang['comets'] = 'visible comets';
 $lang['sun_moon_phenomenon'] = 'sun and moon phenomena';
 $lang['planets_ascension'] = 'right ascension of planets';
 $lang['meteor_streams'] = 'meteor streams';
